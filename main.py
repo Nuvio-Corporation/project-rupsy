d5b94252dd1332424398a4a5391e1485502b4eea
Nuvio <nuvioplanet@gmail.com>
2026-05-04 14:07:20 +0200

    Made initial program

---
+++ main.py
@@ -0,0 +1,20 @@
+on = False
+
+def on_button_pressed_a():
+    global on
+    on = True
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global on
+    on = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_forever():
+    global on
+    if on == True:
+        servos.P0.set_angle(90)
+        basic.pause(500)
+        servos.P0.set_angle(0)
+        basic.pause(500)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+let on = false
+input.onButtonPressed(Button.A, function () {
+    on = true
+})
+input.onButtonPressed(Button.B, function () {
+    on = false
+})
 basic.forever(function () {
-	
+    if (on == true) {
+        servos.P0.setAngle(90)
+        basic.pause(500)
+        servos.P0.setAngle(0)
+        basic.pause(500)
+    }
 })
