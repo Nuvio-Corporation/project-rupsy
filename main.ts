708914a56968e48b3072eaf0c6e65924924652a5
HayWireNickel20 <feddevankrunkelsven@leerling.wico.be>
2026-05-11 14:10:13 +0200

    Heil Hitla

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 let on = false
 input.onButtonPressed(Button.A, function () {
-    on = true
-    basic.showIcon(IconNames.Yes)
+	
 })
 input.onButtonPressed(Button.B, function () {
     on = false
     basic.showIcon(IconNames.No)
+})
+input.onSound(DetectedSound.Loud, function () {
+    on = true
+    basic.showIcon(IconNames.Yes)
 })
 basic.forever(function () {
     if (on == true) {
